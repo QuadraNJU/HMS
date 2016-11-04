5d9c703fda68566a1171dc039e2ec4b3d8b6fcf9
Monkey Liang <i@aq741.com>
2016-11-05 03:05:22 +0800

    add a simple mock object and unit test (一脸懵逼中，哪来的Mock Object)

---
+++ server/src/main/java/nju/quadra/hms/bl/mockObject/MockHotelBL.java
@@ -0,0 +1,10 @@
+package nju.quadra.hms.bl.mockObject;
+
+import nju.quadra.hms.bl.stub.HotelBLService_Stub;
+
+/**
+ * Created by adn55 on 16/11/5.
+ */
+public class MockHotelBL extends HotelBLService_Stub {
+
+}
---
+++ server/src/test/java/nju/quadra/hms/bl/HotelBLTest.java
@@ -0,0 +1,65 @@
+package nju.quadra.hms.bl;
+
+import nju.quadra.hms.bl.mockObject.MockHotelBL;
+import nju.quadra.hms.blservice.hotelBL.HotelBLService;
+import nju.quadra.hms.model.ResultMessage;
+import nju.quadra.hms.vo.HotelVO;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by adn55 on 16/11/5.
+ */
+public class HotelBLTest {
+    HotelBLService hotelBL;
+
+    @Before
+    public void init() {
+        this.hotelBL = new MockHotelBL();
+    }
+
+    @Test
+    public void testSearch() {
+        assertNotNull(hotelBL.search(0));
+    }
+
+    @Test
+    public void testGetAll() {
+        assertNotNull(hotelBL.getAll());
+    }
+
+    @Test
+    public void testGetDetail() {
+        assertNotNull(hotelBL.getDetail(0));
+    }
+
+    @Test
+    public void testAdd() {
+        HotelVO vo = new HotelVO(1, "南京抵抗军会议大酒店", 1, 1, "玄武区中山陵四方城2号",
+                "城中山林花园酒店", "各类客房, 大小会议室", "quadra");
+        assertEquals(ResultMessage.RESULT_SUCCESS, hotelBL.add(vo).result);
+    }
+
+    @Test
+    public void testDelete() {
+        HotelVO vo = new HotelVO(1, "南京抵抗军会议大酒店", 1, 1, "玄武区中山陵四方城2号",
+                "城中山林花园酒店", "各类客房, 大小会议室", "quadra");
+        assertEquals(ResultMessage.RESULT_SUCCESS, hotelBL.delete(vo.id).result);
+    }
+
+    @Test
+    public void testModify() {
+        HotelVO vo = new HotelVO(1, "南京抵抗军会议大酒店", 1, 1, "玄武区中山陵四方城2号",
+                "城中山林花园酒店", "各类客房, 大小会议室", "quadra");
+        assertEquals(ResultMessage.RESULT_SUCCESS, hotelBL.modify(vo.id, vo).result);
+    }
+
+    @Test
+    public void testChangeStaff() {
+        HotelVO vo = new HotelVO(1, "南京抵抗军会议大酒店", 1, 1, "玄武区中山陵四方城2号",
+                "城中山林花园酒店", "各类客房, 大小会议室", "quadra");
+        assertEquals(ResultMessage.RESULT_SUCCESS, hotelBL.changeStaff(vo.id, "hotelstaff").result);
+    }
+}
