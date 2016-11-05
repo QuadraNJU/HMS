c6d982412827c925c5327803510b51608bb14084
jsontom <chen601206301@qq.com>
2016-11-05 16:46:36 +0800

    Revert "Add mock object of promotionBL and"
    This reverts commit 1ff011e3a9c820d165affa1d4ac40f017eb5f9e8.

--- server/src/main/java/nju/quadra/hms/bl/mockObject/MockCreditRecordBL.java
+++
@@ -1,9 +0,0 @@
-package nju.quadra.hms.bl.mockObject;
-
-import nju.quadra.hms.bl.stub.CreditRecordBLService_Stub;
-/**
- * Created by Ray on 16/11/5.
- */
-public class MockCreditRecordBL extends CreditRecordBLService_Stub{
-    
-}
--- server/src/main/java/nju/quadra/hms/bl/mockObject/MockCustomerBL.java
+++
@@ -1,9 +0,0 @@
-package nju.quadra.hms.bl.mockObject;
-
-import nju.quadra.hms.bl.stub.CustomerBLService_Stub;
-/**
- * Created by Ray on 16/11/5.
- */
-public class MockCustomerBL extends CustomerBLService_Stub{
-
-}
