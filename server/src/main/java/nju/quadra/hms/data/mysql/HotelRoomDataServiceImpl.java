d5c9a326cd0d186003e5f5fd9e0aa5cbede4bf60
RaUkonn <574384564@qq.com>
2016-11-16 21:16:21 +0800

    好像弄懂了...

--- server/src/main/java/nju/quadra/hms/data/mysql/HotelRoomDataServiceImpl.java
+++ server/src/main/java/nju/quadra/hms/data/mysql/HotelRoomDataServiceImpl.java
@@ -2,6 +2,7 @@ package nju.quadra.hms.data.mysql;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Types;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
@@ -37,7 +38,10 @@ public class HotelRoomDataServiceImpl implements HotelRoomDataService{
 	public void insert(HotelRoomPO po) throws Exception {
 		PreparedStatement pst = MySQLManager.getConnection()
                 .prepareStatement("INSERT INTO `hotelroom` (`id`, `hotelid`, `name`, `total`, `price`) VALUES (?, ?, ?, ?, ?)");
-		pst.setInt(1, po.getId());
+		if (po.getId() > 0)
+			pst.setInt(1, po.getId());
+		else
+			pst.setNull(1, Types.INTEGER);
         pst.setInt(2, po.getHotelId());
         pst.setString(3, po.getName());
         pst.setInt(4, po.getTotal());
@@ -60,16 +64,18 @@ public class HotelRoomDataServiceImpl implements HotelRoomDataService{
 
 	@Override
 	public void update(HotelRoomPO po) throws Exception {
-		PreparedStatement pst = MySQLManager.getConnection()
-				.prepareStatement("UPDATE `hotelroom` SET `total` = ?, price = ? WHERE `hotelid` = ? AND `name` = ?");
-		pst.setInt(1, po.getTotal());
-		pst.setDouble(2, po.getPrice());
-		pst.setInt(3, po.getHotelId());
-		pst.setString(4, po.getName());
-		int result = pst.executeUpdate();
-		if (result == 0) {
-			throw new Exception("HotelRoom not found");
-		}
+//		PreparedStatement pst = MySQLManager.getConnection()
+//				.prepareStatement("UPDATE `hotelroom` SET `total` = ?, price = ? WHERE `hotelid` = ? AND `name` = ?");
+//		pst.setInt(1, po.getTotal());
+//		pst.setDouble(2, po.getPrice());
+//		pst.setInt(3, po.getHotelId());
+//		pst.setString(4, po.getName());
+//		int result = pst.executeUpdate();
+//		if (result == 0) {
+//			throw new Exception("HotelRoom not found");
+//		}
+		delete(po);
+		insert(po);
 	}
 
 }
