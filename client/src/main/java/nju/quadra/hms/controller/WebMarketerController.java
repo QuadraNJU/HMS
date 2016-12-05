9a6b79bb45cf23660d76d48043b717505e6121e6
Monkey Liang <i@aq741.com>
2016-12-05 21:54:01 +0800

    update WebMarketerController

--- client/src/main/java/nju/quadra/hms/controller/WebMarketerController.java
+++ client/src/main/java/nju/quadra/hms/controller/WebMarketerController.java
@@ -1,8 +1,10 @@
 package nju.quadra.hms.controller;
 
 import nju.quadra.hms.blservice.CreditRecordBLService;
+import nju.quadra.hms.blservice.WebsitePromotionBLService;
 import nju.quadra.hms.model.ResultMessage;
 import nju.quadra.hms.net.BLServiceFactory;
+import nju.quadra.hms.vo.WebsitePromotionVO;
 
 /**
  * Created by adn55 on 2016/12/4.
@@ -10,10 +12,38 @@ import nju.quadra.hms.net.BLServiceFactory;
 public class WebMarketerController {
 
     private CreditRecordBLService creditBL = BLServiceFactory.getCreditRecordBLService();
+    private WebsitePromotionBLService websitePromotionBL = BLServiceFactory.getWebsitePromotionBLService();
 
     public ResultMessage creditTopup(String username, int amount) {
         try {
             return creditBL.topup(username, amount);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResultMessage(ResultMessage.RESULT_NET_ERROR);
+        }
+    }
+
+    public ResultMessage addWebsitePromotion(WebsitePromotionVO vo) {
+        try {
+            return websitePromotionBL.add(vo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResultMessage(ResultMessage.RESULT_NET_ERROR);
+        }
+    }
+
+    public ResultMessage modifyWebsitePromotion(WebsitePromotionVO vo) {
+        try {
+            return websitePromotionBL.modify(vo);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResultMessage(ResultMessage.RESULT_NET_ERROR);
+        }
+    }
+
+    public ResultMessage deleteWebsitePromotion(int id) {
+        try {
+            return websitePromotionBL.delete(id);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResultMessage(ResultMessage.RESULT_NET_ERROR);
